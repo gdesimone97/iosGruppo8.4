ad52c0022e21a6681577926c11c6fb6eba9f26b7
Giuseppe De Simone <44608428+peppo97@users.noreply.github.com>
2019-07-10 16:54:47 +0200

    test connessione1

--- xCode Files/iGuitar/iGuitar WatchKit Extension/InterfaceController.swift
+++ xCode Files/iGuitar/iGuitar WatchKit Extension/InterfaceController.swift
@@ -8,10 +8,55 @@
 
 import WatchKit
 import Foundation
+import WatchConnectivity
 
-
-class InterfaceController: WKInterfaceController {
-
+class InterfaceController: WKInterfaceController, WCSessionDelegate {
+    
+    let session = WCSession.default
+    var sessionStatus = WCSessionActivationState.notActivated
+    var connectionStatus: Bool = false // Stato della connessione, se true può inviare messaggi
+    
+    func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
+        print("Connessione conclusa con successo")
+    }
+    
+    var state = false
+    
+    func ckeckConnection(_ sessionStatus: WCSessionActivationState,_ session: WCSession) -> Bool{
+        
+        var stateConnection = false // Controlla se la connessione è disponibile
+        var reachable = false // Controlla che l'iphone sia sbloccato
+        
+        if sessionStatus == WCSessionActivationState.activated{
+            print("Connessione disponibile")
+            stateConnection = true
+        }
+        else {
+            print("Connessione non disponibile")
+        }
+        
+        if stateConnection && session.isReachable {
+            reachable = true
+        }
+            
+        else {
+            if stateConnection && session.iOSDeviceNeedsUnlockAfterRebootForReachability {
+                print("Sblocca iphone")
+            }
+        }
+        return reachable && stateConnection
+    }
+    
+    func sendMessage(){
+        if connectionStatus {
+            session.sendMessage(["state": state], replyHandler: nil, errorHandler: nil)
+        }
+        else {
+            print("Connessione non disponibile")
+        }
+    }
+    
+    
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
         
@@ -21,11 +66,23 @@ class InterfaceController: WKInterfaceController {
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
+        if WCSession.isSupported() {
+            print("Conessione supportata")
+            session.delegate = self
+            session.activate()
+            sessionStatus = session.activationState
+        }
+        else {
+            print("Connessione non supportata")
+        }
+        
+        connectionStatus = ckeckConnection(sessionStatus, session)
+        
     }
     
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
     }
-
+    
 }
---
+++ xCode Files/iGuitar/iGuitar/SessionConnectivityIphone.swift
@@ -0,0 +1,40 @@
+//
+//  SessionConnectivityIphone.swift
+//  iGuitar
+//
+//  Created by Giuseppe De Simone on 10/07/2019.
+//  Copyright © 2019 Giuseppe De Simone. All rights reserved.
+//
+
+import UIKit
+import WatchConnectivity
+
+class SessionConnectivityIphone: NSObject, WCSessionDelegate {
+    
+    let session = WCSession.default
+    var sessionStatus = WCSessionActivationState.notActivated // Stato della sessione
+    
+    override init() {
+        super.init()
+        session.delegate = self
+        session.activate()
+        sessionStatus = session.activationState
+    }
+    
+    func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
+        print("Sessione Conclusa")
+    }
+    
+    func sessionDidBecomeInactive(_ session: WCSession) {
+        print("Sessione Inattiva")
+    }
+    
+    func sessionDidDeactivate(_ session: WCSession) {
+        print("Sessione Disattivata")
+    }
+    
+    
+    func session(_ session: WCSession, didReceiveMessage message: [String : Any], replyHandler: @escaping ([String : Any]) -> Void) {
+        // Da completare
+    }
+}
