ed8d866d3d57933b7740cb25d610ed63c6143090
Giuseppe De Simone <44608428+peppo97@users.noreply.github.com>
2019-07-10 15:41:45 +0200

    Creazione progetto

---
+++ xCode Files/iGuitar/iGuitar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iGuitar
+//
+//  Created by Giuseppe De Simone on 10/07/2019.
+//  Copyright © 2019 Giuseppe De Simone. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
