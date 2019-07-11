73c326dbac59d49865962d0f886d6be995098911
Giuseppe De Simone <44608428+peppo97@users.noreply.github.com>
2019-07-11 23:13:41 +0200

    Estesto viewController

--- xCode Files/iGuitar/iGuitar/SessionManagerIphone.swift
+++ xCode Files/iGuitar/iGuitar/SessionManagerIphone.swift
@@ -11,47 +11,48 @@ import WatchConnectivity
 
 
 
-class SessionManagerIphone: NSObject,WCSessionDelegate {
+class SessionManagerIphone: NSObject {
     
-    var session = WCSession.default
-    var sessionStatus: WCSessionActivationState {
-        get {
-            return session.activationState
-        }
-    }
-    static var share = SessionManagerIphone()
-    
-    override init() {
-        super.init()
-        session.delegate = self
-        session.activate()
-        sleep(2)
-    }
-    
-    func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
-        if(activationState == WCSessionActivationState.activated){
-            print("Sessione Attivata")
-        }
-        else {
-            print("Sessione non attiva")
-        }
-    }
-    
-    func sessionDidBecomeInactive(_ session: WCSession) { print("Sessione Inattiva") }
-    
-    func sessionDidDeactivate(_ session: WCSession) { print("Sessione Disattivata") }
-    
-    func session(_ session: WCSession, didReceiveMessage message: [String : Any]) {
-        print("Messaggio rivecuto")
-//        messageReceived = message["action"] as! Bool
-//        self.count+=1
-//        DispatchQueue.main.async {
-//            self.label.text = String(self.messageReceived!) + " " + String(self.count)
+//
+//    var session = WCSession.default
+//    var sessionStatus: WCSessionActivationState {
+//        get {
+//            return session.activationState
 //        }
-    }
-    
-    class func reconnect(){
-        self.share = SessionManagerIphone()
-    }
+//    }
+//    static var share = SessionManagerIphone()
+//
+//    override init() {
+//        super.init()
+//        session.delegate = self
+//        session.activate()
+//        sleep(2)
+//    }
+//
+//    func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
+//        if(activationState == WCSessionActivationState.activated){
+//            print("Sessione Attivata")
+//        }
+//        else {
+//            print("Sessione non attiva")
+//        }
+//    }
+//
+//    func sessionDidBecomeInactive(_ session: WCSession) { print("Sessione Inattiva") }
+//
+//    func sessionDidDeactivate(_ session: WCSession) { print("Sessione Disattivata") }
+//
+//    func session(_ session: WCSession, didReceiveMessage message: [String : Any]) {
+//        print("Messaggio rivecuto")
+////        messageReceived = message["action"] as! Bool
+////        self.count+=1
+////        DispatchQueue.main.async {
+////            self.label.text = String(self.messageReceived!) + " " + String(self.count)
+////        }
+//    }
+//
+//    class func reconnect(){
+//        self.share = SessionManagerIphone()
+//    }
     
 }
--- xCode Files/iGuitar/iGuitar/ViewController.swift
+++ xCode Files/iGuitar/iGuitar/ViewController.swift
@@ -10,16 +10,13 @@ import UIKit
 import WatchConnectivity
 
 class ViewController: UIViewController {
+    var session = WCSession.default
+    
     
     @IBOutlet weak var label: UILabel!
     var messageReceived: Bool?
     var count: Int = 0
-    
-    override func viewDidLoad() {
-        
-        label.text = "start"
-    }
-    
+
     
     @IBAction func reset(_ sender: Any) {
         count = 0
@@ -28,3 +25,40 @@ class ViewController: UIViewController {
     }
 }
 
+extension ViewController: WCSessionDelegate {
+    
+    override func viewDidLoad() {
+        label.text = "start"
+        session.delegate = self
+        session.activate()
+        sleep(2)
+    }
+    
+    var sessionStatus: WCSessionActivationState {
+        get {
+            return session.activationState
+        }
+    }
+
+    func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
+        if(activationState == WCSessionActivationState.activated){
+            print("Sessione Attivata")
+        }
+        else {
+            print("Sessione non attiva")
+        }
+    }
+    
+    func sessionDidBecomeInactive(_ session: WCSession) { print("Sessione Inattiva") }
+    
+    func sessionDidDeactivate(_ session: WCSession) { print("Sessione Disattivata") }
+    
+    func session(_ session: WCSession, didReceiveMessage message: [String : Any]) {
+        print("Messaggio rivecuto")
+                messageReceived = message["action"] as! Bool
+                self.count+=1
+                DispatchQueue.main.async {
+                    self.label.text = String(self.messageReceived!) + " " + String(self.count)
+                }
+    }
+}
