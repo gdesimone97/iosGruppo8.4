9b6d89605aa5fec5d473a41bede9b062f8273dca
Giuseppe De Simone <44608428+peppo97@users.noreply.github.com>
2019-07-11 16:52:05 +0200

    Revert "Merge branch 'master' of https://github.com/peppo97/iosGruppo8.4"
    This reverts commit ad0549957f7e37d76ee6302058edfb58c88792a6, reversing
    changes made to 0c7dcaa2a8dfe53a9ef68e9b5b9c663feaec69a3.

--- xCode Files/LetsPlay/LetsPlay/ViewController.swift
+++ xCode Files/LetsPlay/LetsPlay/ViewController.swift
@@ -8,39 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 1;
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return loopMargin*chords.count;
-    }
-    
-    
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return chords[row%chords.count];
-    }
+class ViewController: UIViewController {
 
-    let loopMargin = 50;
-    let chords = ["DO","RE","MI","FA","SOL","LA","SI"];
-    
-    
-    
-    @IBOutlet var chordsPicker: [UIPickerView]!
-    
-    
-    
     override func viewDidLoad() {
-        
         super.viewDidLoad()
-
-        if let _ = chordsPicker {
-        for chPick in chordsPicker{
-            chPick.dataSource = self;
-            chPick.delegate = self;
-        }
-        }
         // Do any additional setup after loading the view.
     }
 
--- xCode Files/Test/Test WatchKit Extension/MotionManager.swift
+++ xCode Files/Test/Test WatchKit Extension/MotionManager.swift
@@ -15,7 +15,7 @@ import WatchKit
  These contexts can be used to enable application specific behavior.
  */
 protocol MotionManagerDelegate: class {
-    func updatedRead(sound: Bool)
+    func updatedRead(pitch: Double, yaw: Double, sum: Double)
 }
 
 class MotionManager {
@@ -24,17 +24,16 @@ class MotionManager {
     let motionManager = CMMotionManager()
     let queue = OperationQueue()
     
-    var buffer = RunningBuffer(size: 50)
-//    var buffer_pitch = RunningBuffer(size: 50)
-    
-    var flag: Bool = false
-    
     
     // MARK: Application Specific Constants
     
-    let startThreshold = 1.00
-    let soundThreshold = 1.00
-    
+    /*
+    // These constants were derived from data and should be further tuned for your needs.
+    let yawThreshold = 1.95 // Radians
+    let rateThreshold = 5.5 // Radians/sec
+ 
+    let resetThreshold = 5.5 * 0.05 // To avoid double counting on the return swing.
+    */
     
     // The app is using 50hz data and the buffer is going to hold 1s worth of data.
     let sampleInterval = 1.0 / 50
@@ -64,17 +63,8 @@ class MotionManager {
                 print("Encountered error: \(error!)")
             }
             
-            if let data = deviceMotion?.rotationRate {
-                let x = data.x
-                let z = data.z
-                
-                if(!self.flag && x > self.startThreshold && z < -self.startThreshold) {
-                    self.flag = true
-                }
-                
-                if self.flag && deviceMotion != nil {
-                    self.processDeviceMotion(deviceMotion!)
-                }
+            if deviceMotion != nil {
+                self.processDeviceMotion(deviceMotion!)
             }
         }
     }
@@ -90,27 +80,15 @@ class MotionManager {
     func processDeviceMotion(_ deviceMotion: CMDeviceMotion) {
         let rotationRate = deviceMotion.rotationRate
         
+        let yaw = rotationRate.z
+        let pitch = rotationRate.x
         let sum = -rotationRate.x + rotationRate.z
         
+//        delegate!.updatedRead(pitch: pitch, yaw: yaw, sum: sum)
         
-        buffer.addSample(sum)
-//        buffer_pitch.addSample(pitch)
         
-        if !buffer.isFull() {
-            return
-        }
-        flag = false
-        
-        let sumAvg = buffer.sum()/50
-        
-        buffer.reset()
-//        buffer_pitch.reset()
-        
-        delegate!.updatedRead(sound: sumAvg >= soundThreshold)
-        
-//        print("yawMin: " + String(format: "%.3f", yawMin) + " yawMax: " + String(format: "%.3f", yawMax) + " yawAvg: " + String(format: "%.3f", yawAvg) + " - pitchMin: " + String(format: "%.3f", pitchMin) + " pitchMax: " + String(format: "%.3f", pitchMax) + " pitchAvg: " + String(format: "%.3f", pitchAvg))
+        print("yaw: " + String(format: "%.3f", yaw) + " - pitch: " + String(format: "%.3f", pitch) + " - sum: " + String(format: "%.3f", sum))
        
-        
     }
     
 }
