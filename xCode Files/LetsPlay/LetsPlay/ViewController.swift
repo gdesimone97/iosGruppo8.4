60a8e72b3b4484354ce8c38a2213dd1045093393
Giuseppe De Simone <44608428+peppo97@users.noreply.github.com>
2019-07-11 16:51:57 +0200

    Revert "Revert "Merge branch 'master' of https://github.com/peppo97/iosGruppo8.4""
    This reverts commit 37a4142b60cb4c3c8b552c25303ab9f22709ec5d.

--- xCode Files/LetsPlay/LetsPlay/ViewController.swift
+++ xCode Files/LetsPlay/LetsPlay/ViewController.swift
@@ -8,10 +8,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1;
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return loopMargin*chords.count;
+    }
+    
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return chords[row%chords.count];
+    }
 
+    let loopMargin = 50;
+    let chords = ["DO","RE","MI","FA","SOL","LA","SI"];
+    
+    
+    
+    @IBOutlet var chordsPicker: [UIPickerView]!
+    
+    
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
+
+        if let _ = chordsPicker {
+        for chPick in chordsPicker{
+            chPick.dataSource = self;
+            chPick.delegate = self;
+        }
+        }
         // Do any additional setup after loading the view.
     }
 
